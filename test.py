019656c9a462794428de3fb33928f03bf423bb16
tom99763 <69715105+tom99763@users.noreply.github.com>
2021-04-14 03:03:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,17 +3,22 @@ from utils import one_hot_board, Net
 from algorithm import DQN
 import torch
 
+from gym_tictactoe.env import TicTacToeEnv
+from utils import one_hot_board, Net
+from algorithm import DQN
+import torch
 
-def run():
+
+def run(player):
     done = False
     obs = env.reset()
     env.render()
     while not done:
-        if obs[1] == 'X':
+        if obs[1] == player:
             action = int(input())
             obs_, _, done, info = env.step(action)
         else:
-            obs = one_hot_board(env)
+            obs = one_hot_board(obs)
             action = model.act(torch.tensor([obs], dtype=torch.float)).item()
             obs_, _, done, exp = env.step(action)
         obs = obs_
@@ -25,4 +30,7 @@ if __name__ == "__main__":
     env = TicTacToeEnv()
     model = DQN()
     model.load_net('net.pkl')
-    run()
+    print('choose O or X player')
+    player=str(input(''))
+    assert player=='O' or player=='X'
+    run(player)
